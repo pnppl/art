86a046db81d01c3ec3ed770228f9944e9be27c70
pnppl <pnpplpnppl@gmail.com>
2022-03-21 14:19:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function play() {
+	myAudio = new Audio('loop.mp3'); 
+	myAudio.ontimeupdate= function(i) {
+	  if((this.currentTime / this.duration)>0.80){
+		this.currentTime = 0;
+		this.play();
+	  }
+	};
+	myAudio.play();
+}
