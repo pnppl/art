5d439809bf4e84ce1e8518e8ad0facaddb85d4e1
pnppl <pnpplpnppl@gmail.com>
2022-03-21 14:58:08 -0400

    whatever

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function play() {
-	myAudio = new Audio('loop.mp3'); 
+	myAudio = new Audio('../loop.mp3'); 
 	myAudio.ontimeupdate= function(i) {
 	  if((this.currentTime / this.duration)>0.80){
 		this.currentTime = 0;
